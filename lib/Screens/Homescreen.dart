3d76a3013be4b647259dc24a832f0d8d1f98574b
Rahul Mishra <rahulmishra102000@gmail.com>
2021-09-04 11:57:25 +0530

    App bar designing completed.

---
+++ lib/Screens/Homescreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({Key? key}) : super(key: key);
+
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> with SingleTickerProviderStateMixin{
+  late TabController _controller; // This is to control the flow of the tabs.
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Whatsapp Clone"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS"
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+        Text("Camera"),
+        Text("Chats"),
+        Text("Status"),
+        Text("Calls"),
+        ],
+      ),
+    );
+  }
+}
