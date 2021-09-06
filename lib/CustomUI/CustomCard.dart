37eb2e409e8995c67a8205a0a1b9729d4d9018dd
Rahul Mishra <rahulmishra102000@gmail.com>
2021-09-06 16:26:20 +0530

    Icon for group and person added, and made the chat column clickable.

--- lib/CustomUI/CustomCard.dart
+++ lib/CustomUI/CustomCard.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-      ),
-      title: Text(
-        "Rahul Mishra",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Row(children: [
-        Icon(Icons.done_all),
-        SizedBox(
-          width: 3,
-        ),
-        Text(
-          "Hey! how you doing?",
-          style: TextStyle(
-            fontSize: 13,
+    return InkWell(
+      onTap: (){},
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: SvgPicture.asset(
+                "asstes/groups.svg",
+                color: Colors.white10,
+              ),
+              backgroundColor: Colors.blueGrey,
+            ),
+            title: Text(
+              "Rahul Mishra",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Row(children: [
+              Icon(Icons.done_all),
+              SizedBox(
+                width: 3,
+              ),
+              Text(
+                "Hey! how you doing?",
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              ),
+            ]),
+            trailing: Text("18:04"),
           ),
-        ),
-      ]),
-      trailing: Text("18:04"),
+          Padding(padding: const EdgeInsets.only(right: 20, left: 80)),
+          Divider(
+            thickness: 1,
+          )
+        ],
+      ),
     );
   }
 }
